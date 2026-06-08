d3e212ffaeb41aea9709db7568b22adfe54df81c
ProjectMA <manasovaalia08@gmail.com>
2026-06-08 14:25:47 +0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBQ9UYY6uh3CgXzytNwt_r2LCwaXval9T8",
+  authDomain: "tutorapp-95f48.firebaseapp.com",
+  projectId: "tutorapp-95f48",
+  storageBucket: "tutorapp-95f48.firebasestorage.app",
+  messagingSenderId: "559214827748",
+  appId: "1:559214827748:web:c77d90b72d607f6df1b4f1"
+};
